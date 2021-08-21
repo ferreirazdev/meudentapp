4d6fb638fbe063c6627cbc5c94942b8d3ec3e47a
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-08-21 15:25:04 -0300

    home screen update

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { SearchInput } from '../../components/SearchInput';
-import { Container } from './styles';
+import LogoSvg from '../../assets/logo.svg'
+import { 
+  Container,
+  Header,
+  SearchBarWrapper
+} from './styles';
 
 export function Home(){
   return (
     <Container>
-      <SearchInput />
+      <StatusBar 
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <LogoSvg 
+          width="55"
+          style={{ alignSelf: 'center'}}
+        />
+        <SearchBarWrapper>
+          <SearchInput 
+            placeholder="Busque um atendimento"
+          />
+        </SearchBarWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary};
 `;
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  padding: 32px 24px;
+`;
+
+export const SearchBarWrapper = styled.View`
+  margin-top: 15px;
+`;
+
